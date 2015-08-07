bb264e9b9f458e5c36b3a93883f4261d79ae9a89
Jack Yen <jack@jackyen.com>
2015-08-07 16:39:55 -0500

    Changed company model
    Signed-off-by: Jack Yen <jack@jackyen.com>

--- server/api/company/company.model.js
+++ server/api/company/company.model.js
@@ -4,9 +4,9 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var CompanySchema = new Schema({
-    name: String,
-    info: String,
-    active: Boolean
+    name: {type: String, required: true},
+    division: {type: String, required: true},
+    admins: {type: [String], lowercase: true, required: true}
 });
 
 module.exports = mongoose.model('Company', CompanySchema);
