6bc1614d4608d29383fc569ffe295e562ff53c3a
Jack Yen <jack@jackyen.com>
2015-08-08 14:04:56 -0500

    Modify role based on edited admin result
    Signed-off-by: Jack Yen <jack@jackyen.com>

--- server/api/company/company.controller.js
+++ server/api/company/company.controller.js
@@ -2,6 +2,7 @@
 
 var _ = require('lodash');
 var Company = require('./company.model');
+var User = require('../user/user.model');
 
 // Get list of companys
 exports.index = function (req, res) {
@@ -48,7 +49,19 @@ exports.update = function (req, res) {
         if (!company) {
             return res.status(404).send('Not Found');
         }
+        _.each(company.admins, function (admin) {
+            if (!_.contains(req.body.admins, admin)) {
+                User.findOneAndUpdate({'email': admin}, {role: 'user'}, function (err) {
+                    if (err) {
+                        return handleError(res, err);
+                    }
+                })
+            }
+        });
         var updated = _.merge(company, req.body);
+        updated.admins = req.body.admins;
+
+
         updated.save(function (err) {
             if (err) {
                 return handleError(res, err);
