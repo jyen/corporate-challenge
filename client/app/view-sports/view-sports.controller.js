3d8042ea26c431c21317d3e3136f34ccabe71888
Jack Yen <jack@jackyen.com>
2015-08-08 21:29:27 -0500

    View sport page
    Signed-off-by: Jack Yen <jack@jackyen.com>

--- client/app/view-sports/view-sports.controller.js
+++ client/app/view-sports/view-sports.controller.js
@@ -1,6 +1,56 @@
 'use strict';
 
-angular.module('corporateChallengeApp')
-  .controller('ViewSportsCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
+(function () {
+  angular.module('corporateChallengeApp').controller('ViewSportsCtrl', ViewSportsCtrl);
+  ViewSportsCtrl.$inject = ['sportService'];
+  function ViewSportsCtrl(sportService) {
+
+    var vm = this;
+    vm.sports = [];
+
+    vm.getCountString = getCountString;
+    vm.getEmailList = getEmailList;
+    init();
+
+    function init() {
+      getSports();
+    }
+
+    function getSports() {
+      var year = new Date().getFullYear();
+      return sportService.getSports(year, true)
+          .then(function (data) {
+            vm.sports = data;
+            return vm.sports;
+          })
+    }
+
+    function getCountString(members) {
+      var genderCount = _.countBy(members, function (member) {
+        return member.gender;
+      });
+
+      var maleCount = 0;
+      var femaleCount = 0;
+      if (genderCount['M']) {
+        maleCount = genderCount['M'];
+      }
+
+      if (genderCount['F']) {
+        femaleCount = genderCount['F'];
+      }
+
+      return 'Male: ' + maleCount + ' Female: ' + femaleCount;
+    }
+
+    function getEmailList(members) {
+      var emails = [];
+      _.each(members, function (member) {
+        emails.push(member.email);
+      });
+      var emailString = emails.join();
+      return 'mailto:' + emailString;
+    }
+  };
+
+})();
--- client/app/view-sports/view-sports.js
+++ client/app/view-sports/view-sports.js
@@ -1,11 +1,12 @@
 'use strict';
 
 angular.module('corporateChallengeApp')
-  .config(function ($stateProvider) {
-    $stateProvider
-      .state('view-sports', {
-        url: '/view-sports',
-        templateUrl: 'app/view-sports/view-sports.html',
-        controller: 'ViewSportsCtrl'
-      });
-  });
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('view-sports', {
+                url: '/view-sports',
+                templateUrl: 'app/view-sports/view-sports.html',
+                controller: 'ViewSportsCtrl',
+                controllerAs: 'viewSport'
+            });
+    });
